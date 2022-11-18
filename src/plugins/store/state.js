3dd60f2d72869c74843871a4a7df7d77097983a1
ivelov <ivelov.vlad1@gmail.com>
2022-11-18 15:21:52 +0200

    refactor: break store in separate files

---
+++ src/plugins/store/state.js
@@ -0,0 +1,57 @@
+
+export default {
+    currentConferenceData: {
+        conference: {
+          id: null,
+          title: "",
+          category_id:null,
+          country: "",
+          date: "",
+          time: "",
+          latitude: 0,
+          longitude: 0,
+        },
+        participant: false,
+        canUpdate: false,
+      },
+      conferencesPageInfo: {
+          maxPage:1,
+      },
+      loading: true,
+      currentReportData:{
+        report:{
+          id:null,
+          conferenceId:null,
+          title:'',
+          description:'',
+          startTime:'',
+          endTime:'',
+          presentation:null,
+          isOnline:false,
+        },
+        canUpdate:false,
+        busyStartTimes:[],
+        busyEndTimes:[],
+        confStartTime:'07:00'
+      },
+      reportsPageInfo: {
+        reports:[],
+        maxPage:1,
+      },
+      commentsInfo:{
+        comments:[],
+        maxPage:1,
+        curPage:0,
+        loading:false
+      },
+      categories:[],
+      categoriesList:[],
+      filters:{},
+      pusher:undefined,
+      channel:undefined,
+      channelLoading: false,
+      meetingsPageInfo:{
+        meetings:{},
+        maxPage:1
+      },
+  };
